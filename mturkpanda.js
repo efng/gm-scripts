a76a494f7dd92e803ef47f7d98e76a6bf183e600
Matthew Gallaer <mgallaer@gmail.com>
2015-02-09 14:42:03 -0800

    reduced it to just one line

--- mturkpanda.js
+++ mturkpanda.js
@@ -4,13 +4,10 @@
 // @namespace  http://efng.github.io/
 // @version    0.1
 // @description reloads any page that that jsut has preview set without accept.
-// @match      https://www.mturk.com/mturk/preview*
+// @match      https://www.mturk.com/mturk/preview\?*
 // @downloadURL https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @updateURL   https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @copyright  2014+, efng
 // @run-at document-start
 // ==/UserScript==
-if ( /preview\?/.test (window.location.href))
-{
-    window.location = window.location.href.replace(/preview\?/,'previewandaccept?')
-}
+window.location = window.location.href.replace(/preview\?/,'previewandaccept?');
