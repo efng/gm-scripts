e7abeb00d4734f276ea2680d3db7c7d62bf842ca
Matthew Gallaer <mgallaer@gmail.com>
2015-02-09 17:26:15 -0800

    script must wait for the page to load

--- reddit.to.hn.user.js
+++ reddit.to.hn.user.js
@@ -2,13 +2,13 @@
 // @name       reddit comments -> hn.com comments
 // @author     efng
 // @namespace  http://efng.github.io/
-// @version    0.1.5
+// @version    0.2.0
 // @description Click through for reddit.com/r/hackernews/comments direct to hn comments
 // @match      http://www.reddit.com/r/hackernews/comments/*
 // @downloadURL https://raw.githubusercontent.com/efng/gm-scripts/master/reddit.to.hn.user.js
 // @updateURL   https://raw.githubusercontent.com/efng/gm-scripts/master/reddit.to.hn.user.js
 // @copyright  2014+, efng
-// @run-at document-start
+// @run-at document-end
 // ==/UserScript==
 
 document.location.href = $('[href^="http://news.ycombinator.com"]').attr('href');
