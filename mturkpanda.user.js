6ea6b9dfa0ee9212a4322a90c80d5bad271e0814
Matthew Gallaer <mgallaer@gmail.com>
2015-02-09 16:08:09 -0800

    fixed @match and added @exclude

--- mturkpanda.user.js
+++ mturkpanda.user.js
@@ -2,9 +2,10 @@
 // @name       Preview & accept for mturk by efng
 // @author     efng
 // @namespace  http://efng.github.io/
-// @version    0.1
+// @version    0.3
 // @description reloads any page that that jsut has preview set without accept.
-// @match      https://www.mturk.com/mturk/preview\?*
+// @match      https://www.mturk.com/mturk/preview*
+// @exclude    https://www.mturk.com/mturk/previewandaccept*
 // @downloadURL https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @updateURL   https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @copyright  2014+, efng
