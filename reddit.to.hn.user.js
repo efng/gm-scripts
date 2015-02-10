9f438786829a1f4bd145890c70ce19316dfc5157
Matthew Gallaer <mgallaer@gmail.com>
2015-02-09 17:18:17 -0800

    changed name and desc

--- reddit.to.hn.user.js
+++ reddit.to.hn.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name       Click through for reddit.com/r/hackernews/comments direct to hn comments
+// @name       reddit comments -> hn.com comments
 // @author     efng
 // @namespace  http://efng.github.io/
-// @version    0.1
-// @description reloads any page that that jsut has preview set without accept.
+// @version    0.1.5
+// @description Click through for reddit.com/r/hackernews/comments direct to hn comments
 // @match      http://www.reddit.com/r/hackernews/comments/*
 // @downloadURL https://raw.githubusercontent.com/efng/gm-scripts/master/reddit.to.hn.user.js
 // @updateURL   https://raw.githubusercontent.com/efng/gm-scripts/master/reddit.to.hn.user.js
