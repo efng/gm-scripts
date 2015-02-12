e1d0bc9a7a69c430e465057ab14c04be61cf496b
Matthew Gallaer <mgallaer@gmail.com>
2015-02-11 23:19:21 -0800

    removed exclude and placed test in script

--- mturkpanda.user.js
+++ mturkpanda.user.js
@@ -2,13 +2,15 @@
 // @name       Preview & accept for mturk by efng
 // @author     efng
 // @namespace  http://efng.github.io/
-// @version    0.3
+// @version    0.4
 // @description reloads any page that that jsut has preview set without accept.
 // @match      https://www.mturk.com/mturk/preview*
-// @exclude    https://www.mturk.com/mturk/previewandaccept*
 // @downloadURL https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @updateURL   https://raw.githubusercontent.com/efng/gm-scripts/master/mturkpanda.user.js
 // @copyright  2014+, efng
 // @run-at document-start
 // ==/UserScript==
-window.location = window.location.href.replace(/preview\?/,'previewandaccept?');
+if ( /preview\?/.test (window.location.href))
+{
+    window.location = window.location.href.replace(/preview\?/,'previewandaccept?')
+}
